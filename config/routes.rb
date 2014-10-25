9eb8020f8a0a44eb8b1e82aea95a636fbc7c5fb1
Keith Ballinger <keith@standardtreasury.com>
2014-10-25 12:15:10 -0700

    route for angular card viewer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'card_viewer/get'
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
---
+++ test/helpers/card_viewer_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class CardViewerHelperTest < ActionView::TestCase
+end
